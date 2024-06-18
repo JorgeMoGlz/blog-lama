51c2c33235bd0d5f66108e3102f468c4597ad9b7
JorgeMoGlz <jorgemoglz95@gmail.com>
2024-06-18 12:06:36 -0600

    Pagina principal completada

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -1,6 +1,14 @@
+import styles from './pagination.module.css';
 
 export const Pagination = () => {
     return (
-        <div>Pagination</div>
+        <div className={styles.container}>
+            <button className={styles.button}>
+                Previous
+            </button>
+            <button className={styles.button}>
+                Next
+            </button>
+        </div>
     )
 }
